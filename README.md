8a10f5883a4f42d5b1566a123cd951ba55c912fb
Eduardo Hopperdietzel <eduardo@Eduardos-MacBook-Pro.local>
2018-10-18 01:50:24 -0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # InvertedSlice
 Divide photos into inverted slices.
+![](https://i.imgur.com/WAVBqP7.jpg)
+![](https://i.imgur.com/3oaFltd.png)
---
+++ js/script.js
@@ -0,0 +1,65 @@
+var image, picker, canvas, slices, download, count, context;
+
+window.onload = function()
+{
+  picker = document.getElementById("filePicker");
+  canvas = document.getElementById("canvas");
+  slices = document.getElementById("slices");
+  count = document.getElementById("count");
+  download = document.getElementById("download");
+  context = canvas.getContext("2d");
+  image = new Image();
+}
+
+function selectPhoto()
+{
+  picker.click();
+}
+
+function loadPhoto(input)
+{
+  if (input.files[0])
+  {
+    var reader = new FileReader();
+    reader.onload = function (e)
+    {
+      image.src = e.target.result;
+      canvas.width = image.width;
+      canvas.height = image.height;
+      canvas.style.display = "block";
+      slices.style.display = "block";
+      download.style.display = "block";
+      setTimeout(function() { divide(); }, 500);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+function divide()
+{
+  var n = count.value;
+
+  if( n <= 0)
+  {
+    context.drawImage(image,0,0);
+    return;
+  }
+
+  var width = image.width / n;
+  var height = image.height;
+
+  for(var i = 0; i < n ; i++)
+  {
+    context.drawImage(image,i*width,0,width,height,image.width - width*(i+1),0,width,height);
+  }
+
+}
+
+function downloadPhoto()
+{
+  var link = document.createElement('a');
+  link.download = 'prisma.png';
+  link.href = canvas.toDataURL()
+  link.click();
+}
